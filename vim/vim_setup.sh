2000f473be8fc4c0f3472d180f8461d18fab0a53
hsinhoyeh <yhh92u@gmail.com>
2016-02-21 16:58:40 +0800

    [vim] refact vim_setup.sh

--- vim/vim_setup.sh
+++ vim/vim_setup.sh
@@ -2,10 +2,6 @@
 #author hsinhoyeh yhh92u@gmail.com
 #goal: this script help you to automatically setup the vim environment under mac
 
-
-ENV=MAC
-#ENV=UBUNTU
-
 VIMPATH=~/.vim
 VIMBKPATH=~/.vim_bk
 
@@ -19,80 +15,53 @@ if [ ! -d $VIMPATH ] ; then
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 fi
 
+fetch() {
+    DST=$1
+    SRC=$2
+    if [ ! -d $DST ] ; then
+        git clone --verbose $SRC $DST
+    fi
+}
+
 #setup color-sampler-pack
-DPATH=$VIMPATH/bundle/Colour-Sampler-Pack
-if [ ! -d $DPATH ] ; then
-    git clone https://github.com/vim-scripts/Colour-Sampler-Pack.git $DPATH
-fi
+fetch $VIMPATH/bundle/Colour-Sampler-Pack https://github.com/vim-scripts/Colour-Sampler-Pack.git
 
 #setup command-t
-DPATH=$VIMPATH/bundle/command-t
-if [ ! -d $DPATH ] ; then
-    git clone https://github.com/wincent/Command-T.git $DPATH
-fi
+fetch $VIMPATH/bundle/command-t https://github.com/wincent/Command-T.git
 
 #setup delimitmate
-DPATH=$VIMPATH/bundle/delmitmate
-if [ ! -d $DPATH ] ; then
-    git clone https://github.com/vim-scripts/delimitMate.vim.git $DPATH
-fi
+fetch $VIMPATH/bundle/delmitmate https://github.com/vim-scripts/delimitMate.vim.git
 
 #setup fugitive
-DPATH=$VIMPATH/bundle/fugitive
-if [ ! -d $DPATH ] ; then
-    git clone https://github.com/tpope/vim-fugitive.git $DPATH
-fi
+fetch $VIMPATH/bundle/fugitive https://github.com/tpope/vim-fugitive.git
 
 #setup nerdtree
-DPATH=$VIMPATH/bundle/nerdtree
-if [ ! -d $DPATH ] ; then
-    git clone https://github.com/scrooloose/nerdtree.git $DPATH
-fi
+fetch $VIMPATH/bundle/nerdtree https://github.com/scrooloose/nerdtree.git
 
 #setup pyflakes
-DPATH=$VIMPATH/bundle/pyflakes
-if [ ! -d $DPATH ] ; then
-    #TODO install pyflake
-    git clone https://github.com/kevinw/pyflakes-vim.git $DPATH
-fi
+#TODO install pyflake
+fetch $VIMPATH/bundle/pyflakes https://github.com/kevinw/pyflakes-vim.git
 
 #setup tagbar
-DPATH=$VIMPATH/bundle/tagbar
-if [ ! -d $DPATH ] ; then
-    git clone git://github.com/majutsushi/tagbar $DPATH
-fi
+fetch $VIMPATH/bundle/tagbar git://github.com/majutsushi/tagbar
 
 #setup vim-powerline
-DPATH=$VIMPATH/bundle/vim-powerLine
-if [ ! -d $DPATH ] ; then
-    git clone https://github.com/Lokaltog/vim-powerline.git $DPATH
-fi
+fetch $VIMPATH/bundle/powerline https://github.com/powerline/powerline.git
 
-if [ "$ENV" == "MAC" ] ; then
-    #install colors-solarized
-    DPATH=$VIMPATH/bundle/vim-colors-solarized 
-    if [ ! -d $DPATH ] ; then 
-        git clone git://github.com/altercation/vim-colors-solarized.git $DPATH
-    fi
-fi 
+#install colors-solarized
+fetch $VIMPATH/bundle/vim-colors-solarized git://github.com/altercation/vim-colors-solarized.git
 
-DPATH=$VIMPATH/bundle/vim-gitgutter
-if [ ! -d $DPATH ] ; then
-    git clone https://github.com/airblade/vim-gitgutter.git $DPATH
-fi
+#install vim-gitgutter
+fetch $VIMPATH/bundle/vim-gitgutter https://github.com/airblade/vim-gitgutter.git
 
-DPATH=$VIMPATH/bundle/vim-go
-if [ ! -d $DPATH ] ; then
-    git clone https://github.com/fatih/vim-go.git $DPATH
-fi
+#install vim-go
+fetch $VIMPATH/bundle/vim-go https://github.com/fatih/vim-go.git
 
-DPATH=$VIMPATH/bundle/syntastic
-if [ ! -d $DPATH ] ; then
-    git clone https://github.com/scrooloose/syntastic.git $DPATH
-fi
+#install syntastic
+fetch $VIMPATH/bundle/syntastic https://github.com/scrooloose/syntastic.git
 
-if [ ! -f ~/.vimrc ] ; then
-     CPWD=`pwd`
-    ln -s $CPWD/.vimrc_mac ~/.vimrc
-fi
+#if [ ! -f ~/.vimrc ] ; then
+#     CPWD=`pwd`
+#    ln -s $CPWD/.vimrc_mac ~/.vimrc
+#fi
 
