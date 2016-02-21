4dfd60adc2b42db2246dd6908fec26f655ea4252
HsinHo Yeh(葉信和) <hsinho_yeh@htc.com>
2016-02-21 10:48:10 +0800

    [shell] add cscope

---
+++ shell/cscope_gen.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+/usr/local/bin/ctags -R $1
+find $1 -type f -name '*.py' \
+    -o -name '*.java' \
+    -o -name '*.[CH]' \
+    -o -name '*.cpp' \
+    -o -name '*.cc' \
+    -o -name '*.c' \
+    -o -name '*.cs' \
+    -o -name '*.asp' \
+    -o -name '*.hpp' \
+    -o -name '*.go' \
+    -o -name '*.swift' \
+    > cscope.files
+#
+# -b: just build
+# -q: create inverted index
+cscope -b -q
+#/usr/local/bin/pycscope.py cscope.files
